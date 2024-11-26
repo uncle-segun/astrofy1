564082d6cfbb1d81f02dcdade750e63ceae09690
Adebanjo Oluwasegun <91882450+uncle-segun@users.noreply.github.com>
2024-11-26 11:52:11 +0100

    Update config.ts
    Meta Tiitle 7 Meta DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Segun Adebanjo | Digital Marketing Specialist';
+export const SITE_DESCRIPTION = 'Segun Adebanjo a Digital Marketing Specialist with over a Decade of Experience, with lots of experience helping small business scale using digital Marketing
+';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
